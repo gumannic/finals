86d0b74c34bde98dc756b529fb26778a392e671b
Nick <nickgumann@bluewin.ch>
2025-05-08 20:15:43 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "bootstrap/dist/css/bootstrap.min.css";
+  </script>
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Movie Watchlist</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/movies">Movies</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  <slot />
+  
---
+++ teil1_abgabe/src/lib/movies.js
@@ -0,0 +1,73 @@
+export default [
+    {
+        title: "Schatten der Galaxis",
+        year: 2023,
+        duration: "128 Minuten",
+        actors: ["Lena Hartmann", "Tom Berger", "Aisha Novak"],
+        poster: "/images/Schatten der Galaxis.png"
+      },
+      {
+        title: "Zeitschleife Alpha",
+        year: 2024,
+        duration: "115 Minuten",
+        actors: ["Maximilian Reuter", "Jana Müller", "Carlos Vega"],
+        poster: "/images/Zeitschleife Alpha.png"
+      },
+      {
+        title: "Die letzte Arche",
+        year: 2022,
+        duration: "137 Minuten",
+        actors: ["Eva Schmidt", "Leon Krüger", "Fatima Al-Masri"],
+        poster: "/images/Die letzte Arche.png"
+    },
+    {
+      title: "Horizont der Träume",
+      year: 2025,
+      duration: "122 Minuten",
+      actors: ["Nico Schneider", "Amelie Koch", "Yu Chen"],
+        poster: "/images/Horizont der Träume.png"
+    },
+    {
+      title: "Code Nemesis",
+      year: 2023,
+      duration: "110 Minuten",
+      actors: ["Selina Bauer", "Jonas Weber", "Ali Khan"],
+        poster: "/images/Code Nemesis.png"
+    },
+    {
+      title: "Kristallsturm",
+      year: 2021,
+      duration: "134 Minuten",
+      actors: ["Tobias Lehmann", "Hannah Vogt", "Markus Albrecht"],
+        poster: "/images/Kristallsturm.png"
+    },
+    {
+      title: "Wächter der Tiefsee",
+      year: 2024,
+      duration: "140 Minuten",
+      actors: ["Zoe Fischer", "Julian Mertens", "Sara El-Gamal"],
+        poster: "/images/Wächter der Tiefsee.png"
+    },
+    {
+      title: "Projekt Aurora",
+      year: 2025,
+      duration: "119 Minuten",
+      actors: ["Mia Lorenz", "David Tran", "Felix Baumgartner"],
+        poster: "/images/Projekt Aurora.png"
+    },
+    {
+      title: "Der ewige Morgen",
+      year: 2023,
+      duration: "125 Minuten",
+      actors: ["Laura König", "Benni Reimann", "Anya Petrov"],
+        poster: "/images/Der ewige Morgen.png"
+    },
+    {
+      title: "Neonstaub",
+      year: 2022,
+      duration: "108 Minuten",
+      actors: ["Kai Hoffmann", "Emily Wagner", "Leonardo Russo"],
+        poster: "/images/Neonstaub.png"
+    }
+  ];
+  
